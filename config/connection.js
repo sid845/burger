c35c718d3481b0dcd6d2d091fb5c665ac9864eab
Siddharth Patel <siddharthpatel845@gmail.com>
2017-08-17 18:25:52 -0400

    added on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
- 
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL){
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    port:3306,
     user: "root",
     password: "tiger",
     database: "burgers_db"
 });
+};
 
 connection.connect();
 
